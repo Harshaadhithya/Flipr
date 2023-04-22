5ea4a3b9363a2235099992666d70ef49221c50f0
Santhanabalan <bala2102002@gmail.com>
2023-04-22 11:53:24 +0530

    Add additional fields to Profile model
    First Name, Last Name, DOB, BIO and profile pic.

--- users/models.py
+++ users/models.py
@@ -12,7 +12,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=50, choices=role_types, default='normal_user')
     # other fields
-
+    first_name = models.CharField(max_length=50,null=True)
+    last_name = models.CharField(max_length=50,null=True)
+    date_of_birth = models.DateField(null=True)
+    bio = models.TextField(max_length=500, null=True, blank=True)
+    # social = models.URLField(max_length=200, blank=True)
+    image = models.ImageField(default='profile_images/default_profile.png',upload_to='profile_images/', blank=True)
+    
     def __str__(self):
-        return self.user.email
+        return f"{self.user.username}'s profile"
 
